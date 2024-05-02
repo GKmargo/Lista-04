7cc74bc2dd34eec3e5b94639293230b0934f77ad
GKmargo <giovanitccamargo@gmail.com>
2024-05-02 11:16:52 -0300

    Cenario 01 - atualizado

--- Cenario01/src/br/edu/up/Programa.java
+++ Cenario01/src/br/edu/up/Programa.java
@@ -1,11 +1,10 @@
 package br.edu.up;
-import java.util.Arrays;
-
 import br.edu.up.Util.Prompt;
 import br.edu.up.modelo.Livro;
 
 public class Programa {
     public static void main(String[] args) throws Exception {
+        
         Livro livro01 = new Livro("1598FHK", "Core Java 2", new String[]{"Cay S. Horstmann", "Gary Cornell"}, " 0130819336", 2005);
         Prompt.imprimir("Livro 01:");
         Prompt.imprimir("Código: " + livro01.getCodigo());
