99e8270a229097a8de967965f82b08e06ca5b5f8
Pedro Henrique Boldori Bier <124646793+Pedrohbbier@users.noreply.github.com>
2024-05-08 10:56:33 -0300

    Ex3 avançando

--- Cenario03/src/br/edu/up/Programa.java
+++ Cenario03/src/br/edu/up/Programa.java
@@ -1,8 +1,14 @@
 package br.edu.up;
 import br.edu.up.Util.Prompt;
+import br.edu.up.tela.tela;;
 
 public class Programa {
+
+
     public static void main(String[] args) throws Exception {
-        Prompt.imprimir("Hello, World!");
+
+        tela tela = new tela();
+
+        tela.execute();
     }
 }
---
+++ Cenario03/src/br/edu/up/controle/CompromissoControle.java
@@ -0,0 +1,26 @@
+package br.edu.up.controle;
+import br.edu.up.modelos.Compromisso;
+
+public class CompromissoControle {
+    
+    public void adicionarCompromisso(String pessoa, String local, String assunto, int hora) {
+
+    Compromisso compromisso = new Compromisso( pessoa ,  local,  assunto, hora);
+        
+    }
+
+    public void consultarCompromisso(String pessoa) {
+
+    }
+
+    public void excluiCompromisso(String pessoa) {
+
+    }
+    
+    public void listarCompromissos() {
+
+    }
+
+ 
+
+}
